9fed999579e11eb19db1a4dcdf2583181923f15d
somename <someemail>
2015-10-01 12:31:06 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -64,7 +64,7 @@ public class TestSudokuVerifier {
 		
 		SudokuVerifier.checkStringLenght(candidate);
 		
-		assertEquals(80, candidate.length());
+		assertEquals(81, candidate.length());
 	}
 	
 	
