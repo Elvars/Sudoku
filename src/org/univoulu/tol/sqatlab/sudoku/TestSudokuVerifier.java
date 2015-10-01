a084a4c5ddb0b93f98d3f0987a85c9f0ea261827
somename <someemail>
2015-10-01 13:02:22 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -83,7 +83,7 @@ public class TestSudokuVerifier {
 	public void testStringSplitter()
 	{
 		
-		candidate = "1234567890";
+		candidate = "123456789";
 		
 		SudokuVerifier.splitString(candidate);
 		
