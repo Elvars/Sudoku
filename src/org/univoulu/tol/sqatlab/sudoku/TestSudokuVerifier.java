3731bfa4e91e0b24572d118e248c5c4e7d08c057
somename <someemail>
2015-10-01 12:27:06 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -57,6 +57,17 @@ public class TestSudokuVerifier {
 	}
 	
 	
+	@Test
+	public void testCandidateSolutionLenght()
+	{
+		candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		SudokuVerifier.checkStringLenght(candidate);
+		
+		assertEquals(81, 81);
+	}
+	
+	
 	
 
 }
