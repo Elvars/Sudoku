fd0b1ec269aa421b383a541b1e9565f537f2d243
somename <someemail>
2015-10-05 15:09:16 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -131,5 +131,19 @@ public class TestSudokuVerifier {
 	}
 	
 	
+	@Test
+	public void testForVertical()
+	{
+		candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		int actual = 0;
+		SudokuVerifier.splitString(candidate);
+		
+		actual = SudokuVerifier.checkHorizontal();
+		
+		assertEquals(1, actual);
+	}
+	
+	
 
 }
