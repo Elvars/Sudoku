be0dc891e6135ae77e062339f92349e676b940b0
somename <someemail>
2015-10-06 15:37:12 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -153,7 +153,6 @@ public class TestSudokuVerifier {
 		//will pass
 		candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		
-		
 		actual = 0;
 		SudokuVerifier.splitStringToArrays(candidate);
 		
